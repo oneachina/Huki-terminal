e36bda37bd4983c303fb67561caf54e45a13a796
oneachina <ruiruixi@hotmail.com>
2024-11-23 17:32:16 +0800

    fix bug:选中文本时如果打字会被覆盖

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 from __future__ import annotations
 
+import os
+import re
+import subprocess
 import sys
 import time
-import os
-import subprocess
+from typing import Any
 
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import os
-import re
+
 from constants import *
 from ui import Ui_MainWindow
-import sys
-import subprocess
 
 path = os.path.splitdrive(os.path.abspath(os.sep))[
-    0] + '\\' if os.name == 'nt' else os.path.abspath(os.sep)
+           0] + '\\' if os.name == 'nt' else os.path.abspath(os.sep)
 os.chdir(path)
 CONFIG = {
     "color": ["white", str],
@@ -34,7 +27,6 @@ entry = f"{path}> "
 
 
 class CustomPlainTextEdit(QtWidgets.QPlainTextEdit):
-
     now_plain = None
     welcome_length = 94
 
@@ -77,10 +69,9 @@ class CustomPlainTextEdit(QtWidgets.QPlainTextEdit):
 
 
 class MyMainForm(QMainWindow, Ui_MainWindow):
-
     name = CONFIG["name"][0]
     version = CONFIG["version"][0]
-    welcome = f"{name} {version}\n{\
+    welcome = f"{name} {version}\n{ \
         LICENSE}\nType 'help' to view help information.\n"
 
     COMMANDS = {
@@ -111,12 +102,17 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
 
         self.text_edit.setGeometry(QtCore.QRect(0, 50, 1521, 671))
         self.text_edit.setStyleSheet("QPlainTextEdit#plainTextEdit"
-                                     "{background-color: rgb(12, 12, 12);font: 13pt \"Cascadia Code\";color:rgb(255, 255, 255);"
+                                     "{background-color: rgb(12, 12, 12);font: 13pt \"Cascadia Code\";color:rgb(255, "
+                                     "255, 255);"
                                      "border-radius:13px;}")
         self.text_edit.setObjectName("plainTextEdit")
 
         self.print(self.welcome)
         self.print(entry, end="")
+
+        # onea start
+        self.text_edit.selectionChanged.connect(self.on_selection_changed)
+        # onea end
 
     def process_command(self, line_text):
         try:
@@ -133,8 +129,8 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
                         processed_args = []
                         for arg in args:
                             processed_args.append(f'"{str(arg)}"')
-                        eval(f"self.{method_name}({\
-                             ', '.join(processed_args)})")
+                        eval(f"self.{method_name}({ \
+                            ', '.join(processed_args)})")
                     except NameError as e:
                         self.error([CMD_NOT_FOUND, COLON, command])
                     except TypeError as e:
@@ -152,7 +148,7 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
                 if res.stdout:
                     self.print([res.stdout.strip()])
                 elif res.stderr:
-                    self.error(res.stderr.strip())
+                    self.error_tuple(res.stderr.strip())
             else:
                 self.error([CMD_NOT_DEFINED, COLON, command])
         except (KeyboardInterrupt, EOFError):
@@ -161,7 +157,7 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
 
         self.print(entry, end="")
 
-    def print(self, value: tuple[str] | str, sep="", end=""):
+    def print(self, value: tuple[str] | str | list[str] | tuple[Any, ...], sep="", end=""):
         self.text_edit.appendPlainText(sep.join(value) + end)
 
     def warning(self, info: tuple[str]):
@@ -190,7 +186,7 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
         cursor.setCharFormat(QtGui.QTextCharFormat())
         self.text_edit.setTextCursor(cursor)
 
-    def error(self, info: tuple[str]):
+    def error(self, info: list[str | Any] | tuple[str] | str | Exception):
         cursor = self.text_edit.textCursor()
         cursor.movePosition(QtGui.QTextCursor.End)
 
@@ -198,7 +194,10 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
         color_format.setForeground(QtGui.QBrush(QtGui.QColor("red")))
         cursor.setCharFormat(color_format)
 
-        cursor.insertText("\n" + "".join(info))
+        if isinstance(info, (list, tuple)):
+            cursor.insertText("\n" + "".join(info))
+        else:
+            cursor.insertText("\n" + info)
 
         cursor.setCharFormat(QtGui.QTextCharFormat())
         self.text_edit.setTextCursor(cursor)
@@ -228,7 +227,8 @@ class MyMainForm(QMainWindow, Ui_MainWindow):
 
         for directory in directories:
             program_path = os.path.join(directory, program_name)
-            if os.path.isfile(program_path) or os.path.isfile(program_path + '.exe') or os.path.exists(path + program_path):
+            if os.path.isfile(program_path) or os.path.isfile(program_path + '.exe') or os.path.exists(
+                    path + program_path):
                 return True
         return False
 
@@ -248,37 +248,37 @@ ls: 列出目录下的文件和目录 - ls
 help: 查看本消息 - help"""
         self.print(info)
 
-    def cd(self, dirname='.'):
+    def cd(self, dir_name='.'):
         global path, entry
         try:
-            if dirname == '..':
-                new_path = os.path.dirname(path + dirname)
+            if dir_name == '..':
+                new_path = os.path.dirname(path + dir_name)
                 os.chdir(new_path)
                 path = new_path
                 entry = path + "> "
-            elif dirname == '.':
+            elif dir_name == '.':
                 self.print(path)
             else:
-                new_path = os.path.join(path, dirname)
+                new_path = os.path.join(path, dir_name)
                 os.chdir(new_path)
                 path = new_path
                 entry = path + "> "
         except Exception as e:
             if e.errno:
                 if e.errno == 30:
-                    self.error(READONLY_FILE, COLON, new_path)
+                    self.error(READONLY_FILE)
                 elif e.errno == 2:
-                    self.error(DIR_NOT_FOUND, COLON, new_path)
+                    self.error(DIR_NOT_FOUND)
             else:
                 self.error(e)
 
-    def mkdir(self, dirname):
+    def mkdir(self, dir_name):
         try:
-            os.mkdir(os.path.join(path, dirname))
+            os.mkdir(os.path.join(path, dir_name))
         except Exception as e:
             if e.errno:
                 if e.errno == 30:
-                    self.error(READONLY_FILE, COLON, path, dirname)
+                    self.error(READONLY_FILE)
             else:
                 self.error(e)
 
@@ -292,14 +292,24 @@ help: 查看本消息 - help"""
         except Exception as e:
             if e.errno:
                 if e.errno == 30:
-                    self.error(READONLY_FILE, COLON, path, filename)
+                    self.error(READONLY_FILE)
                 elif e.errno == 2:
-                    self.error(FILE_NOT_FOUND, COLON, path, filename)
+                    self.error(FILE_NOT_FOUND)
             else:
                 self.error(e)
 
     def ls(self):
         self.print(os.listdir(path), sep=" ")
+
+    # onea start
+    # bug:选中文本时如果打字会被覆盖
+    def on_selection_changed(self):
+        # 如果文本编辑框中有选中文本，则禁用输入
+        if self.text_edit.textCursor().hasSelection():
+            self.text_edit.setReadOnly(True)
+        else:
+            self.text_edit.setReadOnly(False)
+    # onea end
 
 
 class thread(QThread):
